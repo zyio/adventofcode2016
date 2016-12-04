c7672ccb591ab6ea9d9ca27e78c4e54058906b62
Sam Norbury <sam.norbury@gmail.com>
2016-12-04 19:43:58 +0000

    better

--- 3/part2.rb
+++ 3/part2.rb
@@ -1,6 +1,12 @@
-length = File.open("input", "r").readlines.size# {|f| count = f.read.count("\n")}
-p length
-
-file = IO.readlines("input")
-
-#file.each{|f| p f.split(' ').map!{|x| x.to_i}[0]}
+require 'csv'
+length = File.open("input", "r").readlines.size
+$rows = CSV.parse(File.read('input'),{:col_sep => " "})
+$line = 0
+$count = 0
+$triangles=Array.new(length/3){Array.new}
+while $line < length-1 do
+  $triangles[$count] << $rows[$line][0] << $rows[$line+1][0] << $rows[$line+2][0]
+  $line+=3
+  $count+=1
+end
+p $triangles
