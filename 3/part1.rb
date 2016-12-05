889c7051cf00453c06ba0fe4c003416651dc794e
Sam Norbury <sam.norbury@ukfast.co.uk>
2016-12-05 10:41:26 +0000

    Tidying/standarising stuff

--- 3/part1.rb
+++ 3/part1.rb
@@ -1,9 +1,9 @@
-$possible = 0
+possible = 0
 File.open("input", "r") do |f|
   f.each_line do |line|
     sides = line.split(' ')
     sides.map!{|x| x.to_i}.sort!
-    (sides[0] + sides[1] > sides[2]) ? $possible+=1 : next
+    (sides[0] + sides[1] > sides[2]) ? possible+=1 : next
   end
 end
-p $possible
+p possible
--- 3/part2.rb
+++ 3/part2.rb
@@ -1,23 +1,23 @@
 require 'csv'
 length = File.open("input", "r").readlines.size
-$rows = CSV.parse(File.read('input'),{:col_sep => " "})
-$triangles=Array.new(length){Array.new}
-$row=0
-$count = 0
-$line = 0
-while $row <= 2 do
-  while $line < length-1 do
-    $triangles[$count] << $rows[$line][$row] << $rows[$line+1][$row] << $rows[$line+2][$row]
-    $line+=3
-    $count+=1
+rows = CSV.parse(File.read('input'),{:col_sep => " "})
+triangles=Array.new(length){Array.new}
+row=0
+count = 0
+line = 0
+while row <= 2 do
+  while line < length-1 do
+    triangles[count] << rows[line][row] << rows[line+1][row] << rows[line+2][row]
+    line+=3
+    count+=1
   end
-$row+=1
-$line=0
+row+=1
+line=0
 end
 
-$possible = 0
-$triangles.each do |triangle|
+possible = 0
+triangles.each do |triangle|
     sides = triangle.map{|x| x.to_i}.sort
-    (sides[0] + sides[1] > sides[2]) ? $possible+=1 : next
+    (sides[0] + sides[1] > sides[2]) ? possible+=1 : next
 end
-p $possible
+p possible
