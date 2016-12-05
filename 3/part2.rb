38e3c3a0c5f69f2fdd1b47ad9a7a366906585fee
Sam Norbury <sam.norbury@gmail.com>
2016-12-05 07:34:24 +0000

    3b done

--- 3/part2.rb
+++ 3/part2.rb
@@ -1,12 +1,23 @@
 require 'csv'
 length = File.open("input", "r").readlines.size
 $rows = CSV.parse(File.read('input'),{:col_sep => " "})
-$line = 0
+$triangles=Array.new(length){Array.new}
+$row=0
 $count = 0
-$triangles=Array.new(length/3){Array.new}
-while $line < length-1 do
-  $triangles[$count] << $rows[$line][0] << $rows[$line+1][0] << $rows[$line+2][0]
-  $line+=3
-  $count+=1
+$line = 0
+while $row <= 2 do
+  while $line < length-1 do
+    $triangles[$count] << $rows[$line][$row] << $rows[$line+1][$row] << $rows[$line+2][$row]
+    $line+=3
+    $count+=1
+  end
+$row+=1
+$line=0
 end
-p $triangles
+
+$possible = 0
+$triangles.each do |triangle|
+    sides = triangle.map{|x| x.to_i}.sort
+    (sides[0] + sides[1] > sides[2]) ? $possible+=1 : next
+end
+p $possible
